2fd88d2427e3f3b15a8fed5c4ffd1159c47aa5b6
Smorpius <a2193217022@alumnos.uat.edu.mx>
2025-02-27 10:21:18 -0600

    Add user registration fields and enhance user list display

--- lib/vistas/usuario.dart
+++ lib/vistas/usuario.dart
@@ -8,14 +8,42 @@ class UsuarioPage extends StatefulWidget {
 }
 
 class _UsuarioPageState extends State<UsuarioPage> {
+  final TextEditingController _idController = TextEditingController();
   final TextEditingController _nombreController = TextEditingController();
-  final List<String> _usuarios = [];
+  final TextEditingController _apellidoController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nombreUsuarioController =
+      TextEditingController();
+  final TextEditingController _contrasenaController = TextEditingController();
+  final TextEditingController _fechaNacimientoController =
+      TextEditingController();
+  final List<Map<String, String>> _usuarios = [];
 
   void _agregarUsuario() {
     setState(() {
-      if (_nombreController.text.isNotEmpty) {
-        _usuarios.add(_nombreController.text);
+      if (_idController.text.isNotEmpty &&
+          _nombreController.text.isNotEmpty &&
+          _apellidoController.text.isNotEmpty &&
+          _emailController.text.isNotEmpty &&
+          _nombreUsuarioController.text.isNotEmpty &&
+          _contrasenaController.text.isNotEmpty &&
+          _fechaNacimientoController.text.isNotEmpty) {
+        _usuarios.add({
+          'id': _idController.text,
+          'nombre': _nombreController.text,
+          'apellido': _apellidoController.text,
+          'email': _emailController.text,
+          'nombreUsuario': _nombreUsuarioController.text,
+          'contrasena': _contrasenaController.text,
+          'fechaNacimiento': _fechaNacimientoController.text,
+        });
+        _idController.clear();
         _nombreController.clear();
+        _apellidoController.clear();
+        _emailController.clear();
+        _nombreUsuarioController.clear();
+        _contrasenaController.clear();
+        _fechaNacimientoController.clear();
       }
     });
   }
@@ -32,9 +60,70 @@ class _UsuarioPageState extends State<UsuarioPage> {
         child: Column(
           children: [
             TextField(
+              controller: _idController,
+              decoration: InputDecoration(
+                labelText: "ID",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
               controller: _nombreController,
               decoration: InputDecoration(
                 labelText: "Nombre",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _apellidoController,
+              decoration: InputDecoration(
+                labelText: "Apellido",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _nombreUsuarioController,
+              decoration: InputDecoration(
+                labelText: "Nombre de Usuario",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _contrasenaController,
+              decoration: InputDecoration(
+                labelText: "Contraseña",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                obscuringCharacter: '*',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _fechaNacimientoController,
+              decoration: InputDecoration(
+                labelText: "Fecha de Nacimiento",
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -50,7 +139,13 @@ class _UsuarioPageState extends State<UsuarioPage> {
               child: ListView.builder(
                 itemCount: _usuarios.length,
                 itemBuilder: (context, index) {
-                  return ListTile(title: Text(_usuarios[index]));
+                  final usuario = _usuarios[index];
+                  return ListTile(
+                    title: Text('${usuario['nombre']} ${usuario['apellido']}'),
+                    subtitle: Text(
+                      'Email: ${usuario['email']}\nUsuario: ${usuario['nombreUsuario']}\nFecha de Nacimiento: ${usuario['fechaNacimiento']}',
+                    ),
+                  );
                 },
               ),
             ),
